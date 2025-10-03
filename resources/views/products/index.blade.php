b6057ec953f2274bfeebf4d56eeca71ce5c1e2a7
pusatsolusi <pusatsolusi1@gmail.com>
2025-10-03 09:17:21 +0700

    file index tambah paragraf

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <h1>Project Kasir PPLG</h1>
+
+    <hr>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores veritatis totam perspiciatis nostrum quas eum
+        esse, velit placeat. Nobis eius mollitia inventore ducimus, eligendi laborum quae animi placeat ipsum! Quo
+        tempore, odit dolore aliquid ipsam praesentium nemo unde delectus a ipsum vel architecto repudiandae sapiente
+        doloribus pariatur est dolorem quod, quis quam. Numquam accusantium, et qui illum quod quasi veniam beatae sed?
+        Eos quod amet aliquid. Dolore in, libero eligendi commodi, eum qui, sequi voluptate illo saepe vero et soluta.
+        Debitis doloribus vitae blanditiis recusandae impedit eveniet cum est quos ad et consectetur, voluptates quae
+        iusto suscipit illum eos repellat neque nulla? Beatae illum, aspernatur iusto ad alias minus. Fugit autem porro
+        quasi nobis est atque animi repellendus. Eligendi architecto iusto iste fuga perferendis, nemo corrupti eveniet
+        labore rerum rem doloremque, laborum hic, consequuntur ducimus? Cum consequatur repudiandae ab. Quas, earum
+        consequatur in molestias natus consequuntur ad expedita animi magnam!</p>
 </body>
+
 </html>
